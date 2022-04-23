12806ac2bf7e1643ef766f1bb63f0cef65195f32
ZorigtoBadmain <badmainzzz@mail.ru>
2022-04-24 07:20:44 +1000

    removed unnecessary

--- lib/main.dart
+++ lib/main.dart
@@ -31,13 +31,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,15 +45,11 @@ class _MyHomePageState extends State<MyHomePage> {
           
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            SvgPicture.asset("assets/images/delete.svg")
+            SvgPicture.asset("assets/images/delete.svg", 
+            semanticsLabel: 'SVG From asset folder',)
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
